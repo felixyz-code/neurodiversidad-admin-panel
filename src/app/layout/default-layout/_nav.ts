7ff46ce091c71b25457ffe66c98468e72f99bb6e
Enrique Felix <enrique_estrella@hotmail.com>
2026-01-05 12:05:43 -0700

    Initial commit - based on CoreUI Angular Free

---
+++ src/app/layout/default-layout/_nav.ts
@@ -0,0 +1,144 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Neurodiversidad',
+    url: '/dashboard',
+    iconComponent: { name: 'cil-speedometer' },
+    badge: {
+      color: 'info',
+      text: '1.0.0'
+    }
+  },
+  {
+    name: 'Components',
+    title: true
+  },
+  {
+    name: 'Base',
+    url: '/base',
+    iconComponent: { name: 'cil-puzzle' },
+    children: [
+      {
+        name: 'Accordion',
+        url: '/base/accordion',
+        icon: 'nav-icon-bullet'
+      },
+      {
+        name: 'Breadcrumbs',
+        url: '/base/breadcrumbs',
+        icon: 'nav-icon-bullet'
+      }
+    ]
+  },
+  {
+    name: 'Buttons',
+    url: '/buttons',
+    iconComponent: { name: 'cil-cursor' },
+    children: [
+      {
+        name: 'Buttons',
+        url: '/buttons/buttons',
+        icon: 'nav-icon-bullet'
+      },
+      {
+        name: 'Button groups',
+        url: '/buttons/button-groups',
+        icon: 'nav-icon-bullet'
+      },
+      {
+        name: 'Dropdowns',
+        url: '/buttons/dropdowns',
+        icon: 'nav-icon-bullet'
+      },
+      {
+        name: 'Loading Button',
+        url: 'https://coreui.io/angular/docs/components/loading-button/',
+        icon: 'nav-icon-bullet',
+        badge: {
+          color: 'danger',
+          text: 'PRO'
+        },
+        attributes: { target: '_blank' }
+      }
+    ]
+  },
+  {
+    name: 'Notifications',
+    url: '/notifications',
+    iconComponent: { name: 'cil-bell' },
+    children: [
+      {
+        name: 'Alerts',
+        url: '/notifications/alerts',
+        icon: 'nav-icon-bullet'
+      },
+      {
+        name: 'Badges',
+        url: '/notifications/badges',
+        icon: 'nav-icon-bullet'
+      },
+      {
+        name: 'Modal',
+        url: '/notifications/modal',
+        icon: 'nav-icon-bullet'
+      },
+      {
+        name: 'Toast',
+        url: '/notifications/toasts',
+        icon: 'nav-icon-bullet'
+      }
+    ]
+  },
+  {
+    name: 'Widgets',
+    url: '/widgets',
+    iconComponent: { name: 'cil-calculator' },
+    badge: {
+      color: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    title: true,
+    name: 'Extras'
+  },
+  {
+    name: 'Pages',
+    url: '/login',
+    iconComponent: { name: 'cil-star' },
+    children: [
+      {
+        name: 'Login',
+        url: '/login',
+        icon: 'nav-icon-bullet'
+      },
+      {
+        name: 'Register',
+        url: '/register',
+        icon: 'nav-icon-bullet'
+      },
+      {
+        name: 'Error 404',
+        url: '/404',
+        icon: 'nav-icon-bullet'
+      },
+      {
+        name: 'Error 500',
+        url: '/500',
+        icon: 'nav-icon-bullet'
+      }
+    ]
+  },
+  {
+    title: true,
+    name: 'Links',
+    class: 'mt-auto'
+  },
+  {
+    name: 'Docs',
+    url: 'https://coreui.io/angular/docs/',
+    iconComponent: { name: 'cil-description' },
+    attributes: { target: '_blank' }
+  }
+];
